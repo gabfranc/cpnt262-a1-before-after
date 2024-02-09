613602d2a6e724f6b8322093a4e34887f1cfcc0c
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-08 21:36:47 -0700

    changed index

--- client.js
+++ client.js
@@ -1,18 +1,4 @@
 alert("Fruits Are Yummy!");
-// Arrays in fruits
-const fruitArr = ["apple", "banana", "mango", "grapes"];
 
-console.log(fruitArr);
-
-let frArray = [];
-
-fruitArr.forEach(function(fruit) {
-  frArray.push(fruit);
-});
-
-console.log(frArray);
-
-const myHead = "Apple";
-let strOne = "is a fruit that is sweet";
-
-console.log(myHead + strOne);
+const fruitId = document.getElementById('fruitId');
+console.log(fruitId);
