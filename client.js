38236aa4836336aecb145594a71ddc24f4a4af50
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-09 11:46:45 -0700

    changed wordings

--- client.js
+++ client.js
@@ -1,4 +1,14 @@
 alert("Fruits Are Yummy!");
 
+
+let myFruit ="Coconuts are delicious"
+let myNum = 64;
+
 const fruitName = document.getElementById('fruitOrder');
 console.log(fruitName.value);
+
+
+const fruitClass = document.getElementById('fruitClass');
+console.log(myFruit + myNum);
+
+//Increment or decrement a number using ++/—;
