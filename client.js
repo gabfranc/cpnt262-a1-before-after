07922b06c4dff107dc5a313f637c9d8154408f86
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-09 15:16:42 -0700

    add fruitName

--- client.js
+++ client.js
@@ -1,14 +1,45 @@
 alert("Fruits Are Yummy!");
 
-
-let myFruit ="Coconuts are delicious"
-let myNum = 64;
-
-const fruitName = document.getElementById('fruitOrder');
-console.log(fruitName.value);
+//before Values
+let fruitName = "Watermelon";
+let myNum = 48;
 
 
-const fruitClass = document.getElementById('fruitClass');
-console.log(myFruit + myNum);
+function fruitVal() {
+  const fruitBefore = document.getElementById("fruitName");
+  fruitVal.style.backgroundColor = "pink";
+  fruitVal.style.textAlign = "center";
+}
+
+console.log(fruitName);
+
+//function name(fruit){
+  console.log(fruit);
+
+
+//name('banana');
+//function name(fruit){
+  //return name;
+
+
+//name('banana');
+
+//const fruitName = name('banana');
+
+//let myFruit ="Coconuts are delicious"
+//let myNum = 64;
+
+//const fruitName = document.getElementById('fruitOrder');
+//console.log(fruitName.value);
+
+
+//const fruitClass = document.getElementById('fruitClass');
+//console.log(myFruit + myNum);
 
 //Increment or decrement a number using ++/—;
+
+
+// Before Functions
+
+
+// After Functions
