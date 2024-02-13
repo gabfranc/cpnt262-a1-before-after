48ebf9d2a3074f5c712dd2555ad0ce274eb7cbba
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-12 19:17:47 -0700

    changed lyrics

--- client.js
+++ client.js
@@ -1,4 +1,24 @@
-let myNum = 403;
-let strNum = myNum.toString();
+//before and after variables
 
-console.log(strNum);
+// converting a number into a string
+let num = 403;
+let myNum = num.toString();
+
+console.log(myNum);
+
+
+//Incrementing a number
+num++;
+console.log(num);
+
+
+// changing or replacing some of the words with new words
+const myWords = "x marks the spot, where we fell apart";
+
+myWords.replaceAll("x marks", "There goes");
+
+document.getElementById("demo").innerHTML = myWords;
+
+
+
+
