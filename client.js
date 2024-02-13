b355ec93580bc2a64b94e52c7da4d657e4eca945
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-12 23:54:50 -0700

    adding arrays and the document.getElementIds

--- client.js
+++ client.js
@@ -1,29 +1,29 @@
 //before and after variables
-
+// const theDate = new Date();
+//alert("today is" + theDate);
 // converting a number into a string
-let num = 403;
-let myNum = num.toString();
 
+let num = 40;
+let myNum = num.toString();
 console.log(myNum);
 
-
+document.getElementById("numVal").textContent  = num;
 //incrementing a number
 num++;
 console.log(num);
 
-
-
+document.getElementById("incremNum").textContent  = num++;
 // changing or replacing some of the words with new words
 const myWords = "x marks the spot, where we fell apart";
 
 const changedWords = myWords.replace("marks", "crosses out");
 console.log(changedWords);
 
-
-
+document.getElementById("replaceWords").textContent  = changedWords;
 // creating and logging an array of the values of my object.
 
 const obj = {
+
   banana:{
     number: 43
   },
@@ -42,6 +42,8 @@ const obj = {
 const displayResults = Object.keys(obj).map((key) => [key, obj[key]]);
 console.log(displayResults); 
 
+document.getElementById("arrObj").textContent = displayResults;
+
 // removing a key-value pair from an object with the deleted keywords
 
 let music = {
@@ -53,4 +55,13 @@ let music = {
 delete music.classic;
 delete music['country'];
 console.log(music);
+
+
+//styling with js coding
+
+const bodyStyle = document.body.innerHTML;
+
+document.body.style.backgroundColor ="purple";
+
+
 
