a8431ce83ef62c3da96bf7a150c3e579a972085f
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-12 21:09:36 -0700

    more js codes with arrays

--- client.js
+++ client.js
@@ -6,19 +6,49 @@ let myNum = num.toString();
 
 console.log(myNum);
 
-
-//Incrementing a number
+//incrementing a number
 num++;
 console.log(num);
+
 
 
 // changing or replacing some of the words with new words
 const myWords = "x marks the spot, where we fell apart";
 
-myWords.replaceAll("x marks", "There goes");
-
-document.getElementById("demo").innerHTML = myWords;
-
+const changedWords = myWords.replace("marks", "crosses out");
+console.log(changedWords);
 
 
 
+// creating and logging an array of the values of my object.
+
+const obj = {
+  banana:{
+    number: 43
+  },
+  strawberry: {
+    number: 15
+  },
+  mango: {
+    number: 60
+  },
+  watermelon: {
+    number: 16
+  }
+ 
+};
+
+const displayResults = Object.keys(obj).map((key) => [key, obj[key]]);
+console.log(displayResults); 
+
+// removing a key-value pair from an object with the deleted keywords
+
+let music = {
+  'pop' : 'Taylor Swift',
+  'classic': 'Mozart',
+  'country' : 'Blake Shelton'
+};
+
+delete music.classic;
+delete music['country'];
+console.log(music);
